281634aa129ef684bda2514f61584f30bda6e24b
vankata <ivantsoninski@gmail.com>
2023-05-13 13:24:36 +0300

    planets

--- sketch.js
+++ sketch.js
@@ -1,41 +1,71 @@
+class Orbiter {
+  constructor(color, rad, orbitAlt, orbitAngle = 0, orbitAngleMod = 1) {
+    console.log("here", color)
+    this.color=color;
+    this.orbitAngle = orbitAngle;
+    this.orbitAngleMod = orbitAngleMod;
+    this.rad = rad;
+    this.orbitAlt = orbitAlt;
+    this.x = this.orbitAlt * Math.cos(orbitAngle);
+    this.y = this.orbitAlt * Math.sin(orbitAngle);
+  }
 
+  orbit(object) {
+    this.x = object.x + this.orbitAlt * Math.cos(radians(this.orbitAngle));
+    this.y = object.y + this.orbitAlt * Math.sin(radians(this.orbitAngle));
+    this.orbitAngle = (this.orbitAngle + this.orbitAngleMod) % 360;
+  }
 
-let colors = ["blue","red","green","yellow","orange","purple","pink","black","white","brown","grey","cyan","magenta"];
-let color = colors[Math.floor((rand()*colors.length))];
-let radius = 30 + Math.floor(rand()*70);
-
-function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight);
-  window.metadata = () => ({
-    name:`Circle #${token_id}`,
-    description: `This is a ${color} circle with a radius of ${radius}.`,
-
-    image: canvas.elt.toDataURL("image/png"),
-    properties: {
-      "Color": color,
-      "Radius": radius
-    },
-    attributes: [
-      {
-        trait_type: "Color",
-        value: color,
-      },
-      {
-        display_type: "number",
-        trait_type: "Radius",
-        value: radius,
-      },
-    ]
-  })
-  console.log(metadata());
+  display() {
+    noStroke();
+    fill(this.color.replace(' ', ''));
+    ellipse(this.x, this.y, this.rad, this.rad);
+  }
 }
 
+const color_palette = ['Indian Red', 'Light Coral', 'Light Salmon', 'Brown', "Green", 'Dark Orange', 'Tomato', 'Sandy Brown', 'Green Yellow', 'Powder Blue', 'Cornflower Blue', 'Black'];
+const sun = new Orbiter("yellow", 100, 0);
+const n = Math.floor(randm(3,10))
+const planets = Array(n).fill(0).map((_, i) => {return new Orbiter(color_palette[Math.floor(randm(0, color_palette.length))], 10 + i * 5 + randm(-5, 25), 125 + i * 50, i*30, randm(-4,4) || 3)})
+
+function randm(min, max) {
+  return Math.floor(rand() * (max - min) + min);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  windowResized();
+  window.metadata=() => {return {
+    name: `Solar System #${token_id}`,
+    description: `A randomly generated solar system with ${n} planets: ${planets.map(p => `${p.color.startsWith } ${p.color.toLowerCase()} planet with a radius of ${p.rad}`).join(', ')}.`,
+    image: canvas.toDataURL("image/png"),
+    attributes: 
+      [{trait_type: 'Number of Planets', value: n}].concat(
+      planets.map((p, i) => {return {trait_type: `Planet ${i} Color`, value: p.color}})
+      .concat(planets.map((p,i) => {return {trait_type: `Planet ${i} Radius`, value: p.rad}}))),
+    properties: {
+      "Number of Planets": n,
+      ...planets.reduce((acc, p, i) => {
+        acc[`Planet ${i} Color`] = p.color;
+        acc[`Planet ${i} Radius`] = p.rad;
+        return acc;
+      }, {})
+    }
+
+  }}
+  console.log(seed, metadata())
+}
 
 function draw() {
-  background("ivory");
-  stroke(0);
-  fill(color);
-  circle(width/2,height/2,radius/100 * Math.min(width,height))
+  background(0);
+  translate(width / 2, height / 2);
+  scale(min(width, height)/1000)
+  
+  sun.display();
+  for (let i = 0; i < n; i++) {
+    planets[i].orbit(sun);
+    planets[i].display();
+  }
 }
 
 
